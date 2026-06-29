0520ad57f1686b2bb89cb6f57b4440b068c455f5
hideon <lorcapedro883@gmail.com>
2026-06-29 15:42:00 -0400

    feat: implementación de thread safety, estructura de datos personalizada y control de ciclo de vida

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,53 @@
 use std::net::TcpStream;
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
+
+struct ResultadoEscaneo {
+    puerto: u16,
+    abierto: bool,
+}
 
 fn main() {
     let ip_objetivo = "127.0.0.1";
     let puertos = vec![21, 22, 53, 80, 443, 27017, 3306, 8080];
 
-    println!("Iniciando escaneo MULTIHILO en: {}", ip_objetivo);
-    println!("-----------------------------------------------");
+    println!("Iniciando escaneo con canales MPSC en: {}", ip_objetivo);
+    println!("-----------------------------------------------------");
 
-    let mut hilos = vec![];
+    let (tx, rx) = mpsc::channel();
+    let mut total_hilos = 0;
 
     for puerto in puertos {
+        let tx_hilo = tx.clone();
         let ip = ip_objetivo.to_string();
+        total_hilos += 1;
 
-        let handle = thread::spawn(move || {
+        thread::spawn(move || {
             let direccion = format!("{}:{}", ip, puerto);
 
-            match TcpStream::connect_timeout(
-                &direccion.parse().unwrap(),
-                Duration::from_millis(500),
-            ) {
-                Ok(_) => {
-                    println!("Puerto {:<5} ... [ ABIERTO ]", puerto);
-                }
-                Err(_) => {
-                    println!("Puerto {:<5} ... [ CERRADO ]", puerto);
-                }
-            }
+            let abierto =
+                TcpStream::connect_timeout(&direccion.parse().unwrap(), Duration::from_millis(500))
+                    .is_ok();
+
+            tx_hilo.send(ResultadoEscaneo { puerto, abierto }).unwrap();
         });
-
-        hilos.push(handle);
     }
 
-    for hilo in hilos {
-        hilo.join().unwrap();
+    drop(tx);
+
+    for resultado in rx {
+        let estado = if resultado.abierto {
+            "[ ABIERTO ]"
+        } else {
+            "[ CERRADO ]"
+        };
+        println!("Puerto {:<5} ... {}", resultado.puerto, estado);
     }
 
-    println!("------------------------------------");
-    println!("Escaneo multihilo finalizado con éxito.");
+    println!("------------------------------------------------------");
+    println!(
+        "Escaneo centralizado finalizado con éxito. Se analizaron {} puertos",
+        total_hilos
+    );
 }
